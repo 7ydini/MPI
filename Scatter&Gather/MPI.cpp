3d9135fff7ddde00028980bd3578d1171c6da94f
7ydini <61342487+7ydini@users.noreply.github.com>
2021-02-27 14:04:00 +0300

    Create MPI.cpp

---
+++ Scatter&Gather/MPI.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "mpi.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int ProcNum, ProcRank, RecvRank;
+	int m = 1, value = 1;
+	bool F = true;
+	MPI_Status Status;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
+	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+	int* val = new int[ProcNum];
+	for (int i = 0; i < m; i++)
+	{
+
+		if (ProcRank == 0) {
+			
+			MPI_Scatter(val, 1, MPI_INT, &value, 1, MPI_INT, 0, MPI_COMM_WORLD);
+			printf("\nSend 0 to all process \n");
+			
+		}else {
+			MPI_Gather(&value, 1, MPI_INT, val, 1, MPI_INT, 0, MPI_COMM_WORLD);
+			printf("\nMess from %d to 0 process \n", ProcRank);
+			
+		}
+		if (ProcRank == 0) {
+			for (int i = 1; i < ProcNum; i++) {
+				if (val[i] == 0) {
+					F = false;
+				}
+			}
+		}
+		if (ProcRank == 0) {
+			if (F) {
+				printf("\nTrue\n");
+			}
+			else
+				printf("\nFalse\n");
+		}
+
+	}
+	MPI_Finalize();
+}
