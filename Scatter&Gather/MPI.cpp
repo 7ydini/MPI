706320bf059011c94368fd448632a53c1d609937
7ydini <61342487+7ydini@users.noreply.github.com>
2021-03-24 17:43:42 +0300

    Update MPI.cpp

--- Scatter&Gather/MPI.cpp
+++ Scatter&Gather/MPI.cpp
@@ -4,37 +4,45 @@
 
 using namespace std;
 
+// cd C:\Users\8182160\source\repos\MPI\Debug\
+// mpiexec -n 4 MPI.exe
+
 int main(int argc, char* argv[])
 {
 	int ProcNum, ProcRank, RecvRank;
-	int m = 1, value = 1;
+	int m = 1, g = 0;
 	bool F = true;
 	MPI_Status Status;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
 	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
 	int* getValue = new int[ProcNum];
+	int* value = new int[ProcNum];
+	for (int i = 0; i < ProcNum; i++) {
+		getValue[i] = 0;
+		value[i] = 1;
+	}
 	for (int i = 0; i < m; i++)
 	{
-
-		if (ProcRank == 0) {
-			
-			MPI_Scatter(getValue, 1, MPI_INT, &value, 1, MPI_INT, 0, MPI_COMM_WORLD);
-			printf("\nSend 0 to all process \n");
-			
-		}else {
-			MPI_Gather(&value, 1, MPI_INT, getValue, 1, MPI_INT, 0, MPI_COMM_WORLD);
-			printf("\nMess from %d to 0 process \n", ProcRank);
-			
-		}
-		if (ProcRank == 0) {
-			for (int i = 1; i < ProcNum; i++) {
-				if (getValue[i] == 0) {
-					F = false;
-				}
+		for (int j = 0; j < ProcNum; j++) {
+			if (ProcRank == j) {
+				MPI_Scatter(value, 1, MPI_INT, getValue, 1, MPI_INT, j, MPI_COMM_WORLD);
+				printf("\nSend %d to all process \n", j);
+			}
+			else {
+				
+				MPI_Gather(value, 1, MPI_INT, getValue, 1, MPI_INT, j, MPI_COMM_WORLD);
+				printf("\nRequest from %d to %d process \n", ProcRank, j);
 			}
 		}
-		if (ProcRank == 0) {
+		MPI_Reduce(value, getValue, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+		printf("\ngetValue: %d\n", getValue[0]);
+		printf("Summ %d", MPI_SUM);
+			if (getValue[0] != 1) {
+				F = false;
+			}
+
+		if (ProcRank == ProcNum - 1) {
 			if (F) {
 				printf("\nTrue\n");
 			}
