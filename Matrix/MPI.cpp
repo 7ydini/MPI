2ff72dc4b691a3c2f4dc408f0d8142df76aa7028
7ydini <61342487+7ydini@users.noreply.github.com>
2021-04-21 16:49:07 +0300

    Update MPI.cpp

--- Matrix/MPI.cpp
+++ Matrix/MPI.cpp
@@ -20,25 +20,38 @@ int main(int argc, char* argv[])
 	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
 	int** matrix = new int*[n];
 	int** buffmatrix;
-	if (ProcRank == 0) {
-		 buffmatrix = new int* [n];
-		for (int i = 0; i < n; i++) {
-			buffmatrix[i] = new int[n];
-		}
+	buffmatrix = new int* [n];
+	for (int i = 0; i < n; i++) {
+		buffmatrix[i] = new int[n];
 	}
 	for (int i = 0; i < n; i++) {
 		matrix[i] = new int[n];		
 	}
-	for (int k = 0; k < A; k++) {
+	int count = 0;
+	//for (int k = 0; k < A; k++) {
 		for (int j = 0; j < n; j++) {
 			for (int i = 0; i < n; i++) {
 				matrix[i][j] = 1 + rand() % 10;
 			}
 		}
-
-	}
+		for (int j = 0; j < n; j++) {
+			for (int h = 0; h < n; h++) {
+				//int h = 0;
+				if (ProcRank == j) {
+					for (int i = 0; i < n; i++) {
+						count += matrix[h][i] * matrix[i][h];
+					}
+					MPI_Send(&count, count, MPI_INT, 0, j, MPI_COMM_WORLD);
+				}
+				if (ProcRank == 0) {
+					MPI_Recv(&buffmatrix[h][j], 1, MPI_INT, j, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
+				}
+			}
+			//h++;
+		}
+	//}
 	if (ProcRank == 0) {
-		showMatrix(matrix, n);
+		showMatrix(buffmatrix, n);
 	}
 	
 
